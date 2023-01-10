ed240b4117fdbcfea8dd6cce30fa6d74007d2306
Ilia Polozov <IKPolozov@beeline.ru>
2023-01-10 15:11:28 +0300

    remove unnused

--- KitchenHelperFinal/KitchenHelper/Screens/Stove/InteractorStove.swift
+++ KitchenHelperFinal/KitchenHelper/Screens/Stove/InteractorStove.swift
@@ -14,35 +14,5 @@ protocol AnyInteractorStove {
 }
 
 class StoveInteractor: AnyInteractorStove {
-    
-    let notificationCenter = UNUserNotificationCenter.current()
-    
     var presenter: AnyPresenterStove?
-    let context = Heplers.shared.getContext()
-    private var fridgeItems = [FridgeItem]()
-
-    
-    init() {
-        notificationCenter.requestAuthorization(options: [.alert, .sound]) {
-            (permissionGranted, error) in
-            if (!permissionGranted) {
-                print("Permisssion denied")
-            }
-        }
-    }
-    
-    func fetchFridgeItems() {
-    }
-    
-    func deleteItem(item: FridgeItem) {
-        UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: [item.id ?? ""])
-        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [item.id ?? ""])
-        
-        context.delete(item)
-        do {
-            try context.save()
-        } catch {
-            print("error deleting item CoreData")
-        }
-    }
 }
